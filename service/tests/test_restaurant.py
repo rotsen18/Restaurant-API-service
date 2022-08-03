0302131bd11b243ed7fae14eeedcc21822fbe569
Taras Nester <tarasnester414@gmail.com>
2022-08-03 10:22:53 +0300

    added restaurant tests

--- service/tests/test_restaurant.py
+++ service/tests/test_restaurant.py
@@ -1,15 +1,20 @@
-from unittest import TestCase
+from django.test import TestCase
 
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 
+from service.models import Restaurant
 
 RESTAURANT_URL = reverse("service:restaurant-list")
 
 
-class UnauthenticatedApiTests(TestCase):
+def detail_url(task_id: int):
+    return reverse("service:restaurant-detail", args=[task_id])
+
+
+class UnauthenticatedRestaurantApiTests(TestCase):
     def setUp(self):
         self.client = APIClient()
 
@@ -19,11 +24,11 @@ class UnauthenticatedApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
-class AuthenticatedApiTests(TestCase):
+class AuthenticatedRestaurantApiTests(TestCase):
     def setUp(self):
         self.client = APIClient()
         self.user = get_user_model().objects.create_user(
-            "test_User",
+            "user12",
             "test12345"
         )
         self.client.force_authenticate(self.user)
@@ -37,3 +42,19 @@ class AuthenticatedApiTests(TestCase):
         response = self.client.post(RESTAURANT_URL, payload)
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["name"], payload["name"])
+        self.assertEqual(response.data["address"], payload["address"])
+
+    def test_detail_restaurant(self):
+        payload = {
+            "name": "test restaurant name",
+            "address": "test address"
+        }
+        restaurant = Restaurant.objects.create(**payload)
+        url = detail_url(restaurant.id)
+
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["name"], payload["name"])
+        self.assertEqual(response.data["address"], payload["address"])
